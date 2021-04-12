17aaa9d94a42aeb75e38cf58b6ea8abf7721c876
Aquiles96 <aquilesamm@gmail.com>
2021-04-12 19:29:05 -0400

    Change layout of the command.

--- commands/general/uptime.js
+++ commands/general/uptime.js
@@ -1,6 +1,13 @@
 const Discord = require("discord.js");
 const constants = require(process.cwd() + "/configurations/constants.js");
 const path = require("path");
+const os = require("os");
+const packages_json = require(process.cwd() + "/package.json");
+
+String.prototype.capitalize = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+}
+
 module.exports = {
     name: "uptime",
 	path: path.basename(__dirname),
@@ -10,20 +17,20 @@ module.exports = {
 	description: "command.uptime.desc",
     async execute(client, message, args)
     {
-		let uptimeSeconds = (client.uptime / 1000);
-		let uptimeMinutes = (uptimeSeconds / 60);
-		let uptimeHours = (uptimeMinutes / 60);
-		let uptimeDays = (uptimeHours / 24);
+		let uptime_seconds = (client.uptime / 1000);
+		let uptime_minutes = (uptime_seconds / 60);
+		let uptime_hours = (uptime_minutes / 60);
+		let uptime_days = (uptime_hours / 24);
 		
         let embed = new Discord.MessageEmbed();
-		embed.setTitle(client.user.tag);
+		embed.setAuthor(client.users.cache.get(client.config.owner).tag + "\n" + "(" + client.users.cache.get(client.config.owner).id + ")", client.users.cache.get(client.config.owner).displayAvatarURL({ format: "png", dynamic: true, size: 256 }))
+		embed.setTitle(client.user.tag + "\n" + "(" + client.user.id + ")");
         embed.setThumbnail(client.user.displayAvatarURL({ format: "png", dynamic: true, size: 4096 }));
-		embed.addField(":battery: " + client.utils.getTrans(client, message.author, message.guild, "command.uptime.embed.cpuusage") + ":", client.statsCPU.total.toFixed(2) + "%", true);
-		embed.addField(":card_box: " + client.utils.getTrans(client, message.author, message.guild, "command.uptime.embed.ramusage") + ":", (process.memoryUsage().rss / 1000000).toFixed(2) + "MB", true);
-		embed.addField(":satellite: " + client.utils.getTrans(client, message.author, message.guild, "command.uptime.embed.timeconnected") + ":", client.utils.getTrans(client, message.author, message.guild, "command.uptime.embed.timeconnected.field", [uptimeDays.toFixed(0), (uptimeHours.toFixed(0) % 24), (uptimeMinutes.toFixed(0) % 60), (uptimeSeconds.toFixed(0) % 60)]), false); // uptimeDays.toFixed(0) \ (uptimeHours.toFixed(0) % 24) \ (uptimeMinutes.toFixed(0) % 60) \ (uptimeSeconds.toFixed(0) % 60)
-		embed.addField(":calendar_spiral: " + client.utils.getTrans(client, message.author, message.guild, "command.uptime.embed.createddate") + ":", client.user.createdAt.toString(), false);
-        embed.setFooter(client.utils.getTrans(client, message.author, message.guild, "command.uptime.embed.id") + ": " + client.user.id);
-		embed.setColor(0x66b3ff);
+		embed.addField(":gear: " + client.utils.getTrans(client, message.author, message.guild, "command.uptime.result.software") + ":", ":desktop: " + os.version() + " " + os.arch() + "\n" + ":scroll: " + "Node.js " + process.version + " - " + "discord.js v" + packages_json.dependencies["discord.js"]);
+		embed.addField(":file_cabinet: " + client.utils.getTrans(client, message.author, message.guild, "command.uptime.result.resources_usage") + ":", ":battery: " + client.statsCPU.total.toFixed(2) + "%" + " / " + ":card_box: " + (process.memoryUsage().rss / 1000000).toFixed(2) + "MB", true);
+		embed.addField(":satellite: " + client.utils.getTrans(client, message.author, message.guild, "command.uptime.result.time_connected") + ":", client.utils.getTrans(client, message.author, message.guild, "command.uptime.result.time_connected.field", [uptime_days.toFixed(0), (uptime_hours.toFixed(0) % 24), (uptime_minutes.toFixed(0) % 60), (uptime_seconds.toFixed(0) % 60)]), false);
+		embed.addField(":calendar_spiral: " + client.utils.getTrans(client, message.author, message.guild, "command.uptime.result.creation_date") + ":", client.functions.generateDateString(client, message.author, message.guild, client.user.createdAt).capitalize(), false);
+		embed.setColor(0x0580aa);
         return message.channel.send(embed);
     }
 };
