1a525a2851bffce489dc336b474d84d48a4be422
Aquiles96 <aquilesamm@gmail.com>
2021-04-19 10:57:20 -0400

    Remove unnecesary code.

--- commands/general/afk.js
+++ commands/general/afk.js
@@ -11,29 +11,10 @@ module.exports = {
 	usage: "command.afk.usage",
     cooldown: 5,
 	description: "command.afk.desc",
-	//flags: constants.cmdFlags.dontLoad,
 	execute(client, message, args) {
 		let actual_time = new Date().getTime();
         let reason = args.slice(0).join(" ");
 		client.user_data.prepare("INSERT OR REPLACE INTO afk (user_id, name, reason, time) VALUES (?, ?, ?, ?);").run(message.author.id, message.author.tag, reason, actual_time);
 		return message.channel.send(client.utils.getTrans(client, message.author, message.guild, "command.afk.set", [message.author.tag]) + "\n\n" + reason);
-		
-		/*let get_link = "";
-		if (args.length) {
-			for (var argument_index = 0; argument_index < arguments.length; argument_index++) {
-				if (!args[argument_index]) { continue };
-				if (image_filters.some(find_http => args[argument_index].includes(find_http))) {
-					get_link = args.splice(argument_index).toString();
-				}
-			}
-			reason = args.slice(0).join(" ");
-		}*/
-		
-		/*let embed = new Discord.MessageEmbed();
-		embed.setTitle(client.utils.getTrans(client, message.author, message.guild, "command.afk.set", [message.author.tag])); // "**" + message.author.tag + "**" + " esta AFK"
-		if (reason.length) { embed.setDescription(reason); }
-		if (get_link.length) { embed.setImage(get_link); }
-		embed.setColor([255, 255, 0]);
-		return message.channel.send(embed);*/
 	},
 };
--- functions/events/users_afk_handler.js
+++ functions/events/users_afk_handler.js
@@ -11,12 +11,6 @@ async function messageAFK(client, message) {
 		
 		client.user_data.prepare("DELETE FROM afk WHERE user_id = ?").run(message.author.id); //client.user_afk.delete.run(message.author.id);
 		return message.channel.send(afk_message);
-		
-		/*var embed = new Discord.MessageEmbed();
-		embed.setTitle(client.utils.getTrans(client, message.author, message.guild, "afkhandler.title.gone", [message.author.tag]));
-		embed.setFooter("Lasted " + afk_time)
-		embed.setColor([0, 255, 0]);
-		return message.channel.send(embed);*/ // message.author.tag
 	}
 	
 	let users = message.mentions.users.array();
@@ -27,28 +21,6 @@ async function messageAFK(client, message) {
 			let afk_time = client.functions.generateDurationString(client, message.author, message.guild, get_user.time);
 			let afk_message = client.utils.getTrans(client, message.author, message.guild, "afk_handler.pinged", [message.author.tag]) + "\n" + client.utils.getTrans(client, message.author, message.guild, "afk_handler.pinged.time", [afk_time]) + "\n\n" + get_user.reason;
 			return message.channel.send(afk_message);
-			
-			/*let reason = get_user.reason;
-			let reason_array = reason.split(" ");
-			let get_link = "";
-			if (reason_array.length) {
-				for (var argument_index = 0; argument_index < arguments.length; argument_index++) {
-					if (!reason_array[argument_index]) { continue };
-					if (image_filters.some(find_http => reason_array[argument_index].includes(find_http))) {
-						get_link = reason_array.splice(argument_index).toString();
-					}
-				}
-				reason = reason_array.slice(0).join(" ");
-			}*/
-			
-			
-			/*let embed = new Discord.MessageEmbed();
-			embed.setTitle(client.utils.getTrans(client, message.author, message.guild, "afkhandler.title.active", [get_user.name])); // get_user.name
-			if (reason.length) { embed.setDescription(reason); }
-			if (get_link.length) { embed.setImage(get_link); }
-			embed.setFooter(afk_time + "ago.");
-			embed.setColor([255, 255, 0]);
-			return message.channel.send(embed);*/
 		}
 	}
 }
