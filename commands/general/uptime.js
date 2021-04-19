24e9c87840a3c6f3d835f61f3b85e9045db7172f
Aquiles96 <aquilesamm@gmail.com>
2021-04-19 11:33:53 -0400

    Replace old uptime calculation to make use of the new function in core.js.

--- commands/general/uptime.js
+++ commands/general/uptime.js
@@ -17,11 +17,7 @@ module.exports = {
 	description: "command.uptime.desc",
     async execute(client, message, args)
     {
-		let uptime_count = new Date().getTime() - client.readyAt.getTime();
-		let uptime_seconds = (uptime_count / 1000);
-		let uptime_minutes = (uptime_seconds / 60);
-		let uptime_hours = (uptime_minutes / 60);
-		let uptime_days = (uptime_hours / 24);
+		let uptime_count = client.functions.generateDurationString(client, message.author, message.guild, client.readyAt.getTime());
 		
         let embed = new Discord.MessageEmbed();
 		embed.setAuthor(client.users.cache.get(client.config.owner).tag + "\n" + "(" + client.users.cache.get(client.config.owner).id + ")", client.users.cache.get(client.config.owner).displayAvatarURL({ format: "png", dynamic: true, size: 256 }))
@@ -29,7 +25,7 @@ module.exports = {
         embed.setThumbnail(client.user.displayAvatarURL({ format: "png", dynamic: true, size: 4096 }));
 		embed.addField(":gear: " + client.utils.getTrans(client, message.author, message.guild, "command.uptime.result.software") + ":", ":desktop: " + os.version() + " " + os.arch() + "\n" + ":scroll: " + "Node.js " + process.version + " - " + "discord.js v" + packages_json.dependencies["discord.js"]);
 		embed.addField(":file_cabinet: " + client.utils.getTrans(client, message.author, message.guild, "command.uptime.result.resources_usage") + ":", ":battery: " + client.statsCPU.total.toFixed(2) + "%" + " / " + ":card_box: " + (process.memoryUsage().rss / 1000000).toFixed(2) + "MB", true);
-		embed.addField(":satellite: " + client.utils.getTrans(client, message.author, message.guild, "command.uptime.result.time_connected") + ":", client.utils.getTrans(client, message.author, message.guild, "command.uptime.result.time_connected.field", [uptime_days.toFixed(0), (uptime_hours.toFixed(0) % 24), (uptime_minutes.toFixed(0) % 60), (uptime_seconds.toFixed(0) % 60)]), false);
+		embed.addField(":satellite: " + client.utils.getTrans(client, message.author, message.guild, "command.uptime.result.time_connected") + ":", uptime_count, false);
 		embed.addField(":calendar_spiral: " + client.utils.getTrans(client, message.author, message.guild, "command.uptime.result.creation_date") + ":", client.functions.generateDateString(client, message.author, message.guild, client.user.createdAt).capitalize(), false);
 		embed.setColor(0x0580aa);
         return message.channel.send(embed);
