c96d30c42d2e693abc68518fb8fd5440d3d5b5b3
Aquiles96 <aquilesamm@gmail.com>
2021-04-27 00:06:30 -0400

    Re-add missing files.

---
+++ commands/developer/execute.js
@@ -0,0 +1,32 @@
+const Discord = require("discord.js");
+const constants = require(process.cwd() + "/configurations/constants.js");
+const path = require("path");
+const child_process = require("child_process");
+module.exports = {
+    name: "execute",
+	path: path.basename(__dirname),
+    cooldown: 0,
+    flags: constants.cmdFlags.ownerOnly,
+    usage: "<commando>",
+	description: "Ejecutar comandos en el terminal.",
+	async execute(client, message, args) {
+		let result = undefined;
+        let command = args.slice(0).join(" ");
+        if (command == "") {
+			let embed = new Discord.MessageEmbed();
+            embed.setDescription(":no_entry: " + "Comando invalido, no se puede ejecutar.");
+			embed.setColor([255, 0, 0]);
+            return message.channel.send(embed);
+		}
+        try {
+			let result = await child_process.execSync(command);
+			console.log(result.toString());
+		}
+        catch (error) {
+			let embed = new Discord.MessageEmbed();
+			embed.setDescription(error.message);
+			embed.setColor([255, 0, 0]);
+			return message.channel.send(embed);
+		}
+    }
+}; 
--- commands/general/invite.js
+++ commands/general/invite.js
@@ -5,9 +5,14 @@ module.exports = {
 	name: "invite",
 	path: path.basename(__dirname),
     cooldown: 5,
-	description: "command.invite.usage", //"Obtén la invitación del bot.",
+	description: "invite.usage", //"Obtén la invitación del bot.",
 	execute(client, message, args) {
 		let invite_url = "https://discord.com/api/oauth2/authorize?client_id=610988333618823188&permissions=8&scope=bot";
-		return message.channel.send(invite_url);
+		let invite_canary_url = "https://discord.com/api/oauth2/authorize?client_id=836360465180917765&permissions=8&scope=botl";
+		let embed = new Discord.MessageEmbed();
+		embed.setTitle(client.functions.getTranslation(client, message.author, message.guild, "commands_gsettings", "embed.title"));
+		embed.addField(client.functions.getTranslation(client, message.author, message.guild, "commands_gsettings", "embed.version_stable"), "[Stable](" + invite_url + ")");
+		embed.addField(client.functions.getTranslation(client, message.author, message.guild, "commands_gsettings", "embed.version_canary"), "[Canary](" + invite_canary_url + ")");
+		return message.inlineReply(embed);
 	},
 };
