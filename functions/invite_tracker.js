556e97254c6c5168bdbb4566401b32aec78ac457
Lucario-TheAuraPokemon <lucariopokemonaura@gmail.com>
2021-06-05 12:56:00 -0400

    Add slash commands loader and fix invite tracker

--- functions/invite_tracker.js
+++ functions/invite_tracker.js
@@ -1,7 +1,10 @@
 const Discord = require("discord.js");
 async function invite_tracker(client) {
-	//console.log("");
 	await client.guild_invites.forEach(async function(guild) { await client.guild_invites.delete(guild); });
+	
+	let invite_count = 0;
+	let guilds_count = 0;
+	
 	var guilds_array = client.guilds.cache.array();
 	for (var guild_index = 0; guild_index < guilds_array.length; guild_index += 1) {
 		var guild_data = client.guild_invites.get(guilds_array[guild_index].id);
@@ -9,13 +12,16 @@ async function invite_tracker(client) {
 			client.guild_invites.set(guilds_array[guild_index].id, new Discord.Collection());
 			guild_data = client.guild_invites.get(guilds_array[guild_index].id);
 		}
+		guilds_count++;
 		
 		await guilds_array[guild_index].fetchInvites().then(async (invites) => {
 			var invites_array = invites.array();
 			for (var invite_index = 0; invite_index < invites_array.length; invite_index += 1) {
 				guild_data.set(invites_array[invite_index].code, invites_array[invite_index].uses);
+				invite_count++;
 			}
 		});
 	}
+	//console.log("Cached " + guilds_count + " guilds and " + invite_count + " invites.");
 }
 module.exports = invite_tracker;
