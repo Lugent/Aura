654781b27d59345216898be6be60bef0ef9b7589
Lucario-TheAuraPokemon <lucariopokemonaura@gmail.com>
2021-06-05 12:57:50 -0400

    Update main file

--- main.js
+++ main.js
@@ -25,7 +25,21 @@ let dotenv_result = dotenv.config();
 if (dotenv_result.error) { process.exit(); } else { console.log("Loaded enviroment variables."); }
 
 // Client
-const client = new Discord.Client({intents: Discord.Intents.ALL, presence: {status: "invisible"}, fetchAllMembers: true, http: {version: 7}});
+const client = new Discord.Client({
+	allowedMentions: {
+		repliedUser: false
+	}, 
+	intents: Discord.Intents.ALL, 
+	presence: {
+		status: "invisible"
+	},
+	fetchAllMembers: true,
+	http: {
+		version: 7
+	}
+});
+
+// Setup client
 //const buttons = require('discord-buttons')(client);
 //client.buttons = buttons;
 client.config = require(process.cwd() + "/configurations/client.js");
@@ -65,6 +79,7 @@ client.functions.getTranslation = require(process.cwd() + "/functions/translator
 
 // Client data
 client.commands = new Discord.Collection();
+client.slash_commands = new Discord.Collection();
 client.cooldowns = new Discord.Collection();
 client.exp_cooldowns = new Discord.Collection();
 client.guild_invites = new Discord.Collection();
@@ -99,6 +114,7 @@ console.log("Connecting to Discord...");
 
 // Load event files
 let general_command_executor = require(process.cwd() + "/events/general_command_executor.js"); 
+let general_slash_command_executor = require(process.cwd() + "/events/general_slash_command_executor.js"); 
 let general_data_handler = require(process.cwd() + "/events/general_database_handler.js");
 let guild_invite_tracker = require(process.cwd() + "/functions/invite_tracker.js");
 let guild_msg_logger = require(process.cwd() + "/events/general_message_logger.js");
@@ -107,6 +123,10 @@ let guild_experience_handler = require(process.cwd() + "/events/guilds_experienc
 let user_afk_handler = require(process.cwd() + "/events/users_afk_handler.js");
 
 // Execute events
+client.on("interaction", async (interaction) => {
+	general_slash_command_executor(client, interaction);
+});
+
 client.on("message", async (message) => {
 	await general_data_handler(client, message);
 	
@@ -202,19 +222,19 @@ client.on("guildCreate", async (guild) => {
 	await guild_bot_welcome(client, guild);
 	await guild_invite_tracker(client);
 	console.log("Joined to guild " + guild.name);
-	console.log("<" + guild.id + ">");
+	console.log("ID: " + guild.id);
 });
 
 client.on("guildDelete", async (guild) => {
 	await guild_invite_tracker(client);
-	console.log("Leaved from guild " + guild.name);
-	console.log("<" + guild.id + ">");
+	console.log("Exited from guild " + guild.name);
+	console.log("ID: " + guild.id);
 });
 
 client.on("guildUnavailable", async (guild) => {
 	if (guild.name) {
 		console.log("A guild is unavaliable -> " + guild.name);
-		console.log("<" + guild.id + ">");
+		console.log("ID: " + guild.id);
 	}
 });
 
@@ -236,14 +256,21 @@ client.on("guildUnavailable", async (guild) => {
 
 // Verbose
 let command_loader = require(process.cwd() + "/functions/command_loader.js");
+let slash_command_loader = require(process.cwd() + "/functions/slash_command_loader.js");
 client.on("ready", async () => {
 	client.functions.resourceMonitor(client);
 	
 	console.log("Registering commands...");
 	await command_loader(client);
+	console.log("");
+
+	console.log("Registering slash commands...");
+	await slash_command_loader(client);
+	console.log("");
 	
 	console.log("Initializing invite tracker...");
 	await guild_invite_tracker(client);
+	console.log("");
 	
 	if (client.status_updating) { client.functions.activityUpdater(client); }
 	setInterval(function() { if (client.status_updating) { client.functions.activityUpdater(client); }}, 15000);
@@ -261,7 +288,7 @@ client.on("ready", async () => {
 	}, 120000);
 	
 	client.connected = true;
-	console.log("Functions ready.");
+	console.log("Everything is ready!");
 	console.log("Connected as " + client.user.tag);
 });
 client.on("invalidated", () => {
