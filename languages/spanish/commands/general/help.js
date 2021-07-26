c31a860f58606e63a149b702dc9c9d3dfc9cb27a
Lucario-TheAuraPokemon <lucariopokemonaura@gmail.com>
2021-07-26 14:03:22 -0400

    Add some missing translations.

--- languages/spanish/commands/general/help.js
+++ languages/spanish/commands/general/help.js
@@ -1,12 +1,31 @@
 let language = {
+	// Core
+	"embed.title": "Lista de categorias",
+	"embed.footer": "Usa el scroll de abajo para seleccionar categoria",
+	"select_menu": "Seleccione categoria",
+	
+	// Category
+	"embed.category.title": "Categoria de %s",
+	"embed.category.footer": "Usa el scroll de abajo para seleccionar otra categoria",
+	
+	// Categories
 	"category.administration": "Administración",
 	"category.fun": "Diversión",
 	"category.general": "General",
 	"category.information": "Información",
 	"category.searching": "Busqueda",
 	"category.utilities": "Utilidades",
-	"list.title": "Lista de todos los comandos",
-	"list.footer": "Usa %shelp [comando] para información detallada de un comando",
+	
+	// Commands
+	// Administration
+	"language.description": "Cambia el lenguaje del bot.",
+	
+	// General
+	"help.usage": "[comando]",
+	"help.description": "Obtén ayuda de los comandos.",
+	
+	"invite.usage": "Obtén una invitación al bot",
+	
 	"success_list": "Lista de comandos enviado",
 	"failure_list": "Imposible enviarte una lista de comandos",
 	"aliases": "Alias",
