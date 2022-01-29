9e802d5c15fbf55801af24b39f15079b0dcd8bf7
Lugent <lucariopokemonaura@gmail.com>
2022-01-29 13:07:00 -0400

    Change xp formula since is too unfair

--- configurations/client.js
+++ configurations/client.js
@@ -1,11 +1,11 @@
 const math = require("mathjs");
 const config = {
 	commands_dir: "commands",
-	exp_score_base: 48,
+	exp_score_base: 25,
 	exp_level_max: 200,
 	exp_gain_rate: 4,
 	exp_cooldown: 10000,
-	exp_exponent: 1.6,
+	exp_exponent: 1.5,
 };
 config.exp_formula = function(level) { return math.floor(level * (config.exp_score_base ** config.exp_exponent)); }
 module.exports = config;
